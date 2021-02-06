188d6c445abe10efb82dc200bf5cfff88887a099
Temitope Adebiyi <adebiyitope26@gmail.com>
2021-02-06 23:39:31 +0100

    sign/login page

---
+++ food app/Extensions/UiView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UiView+Extension.swift
+//  food app
+//
+//  Created by Temitope on 06/02/2021.
+//
+
+import Foundation
---
+++ food app/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  OnboardingViewController.swift
+//  food app
+//
+//  Created by Temitope on 06/02/2021.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var pageControl: UIPageControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func nextBtnClicked(_ sender: Any) {
+    }
+    
+}
