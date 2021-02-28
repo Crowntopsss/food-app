22888512b679f15747ffa198156a33a38b729fe9
Temitope Adebiyi <adebiyitope26@gmail.com>
2021-02-28 22:53:43 +0100

    pods install

---
+++ food app/CustomViews/CollectionViewCalls/CategoryCollectionViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryCollectionViewCell.swift
+//  food app
+//
+//  Created by Temitope on 28/02/2021.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLbl: UILabel!
+    
+    func setup(category: DishCategory){
+        
+    }
+}
+
---
+++ food app/Models/DishCategory.swift
@@ -0,0 +1,12 @@
+//
+//  DishCategory.swift
+//  food app
+//
+//  Created by Temitope on 28/02/2021.
+//
+
+import Foundation
+
+struct DishCategory {
+    let id,name,image: String
+}
